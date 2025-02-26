de8d13116cd2c78fb8e0fcb6d1274e52e03b765c
김예찬 <ycraah@gamil.com>
2025-02-26 12:19:52 +0900

    TodoList 컴포넌트 생성성

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import "./App.css";
 import Header from "./component/Header.js";
 import TodoEditor from "./component/TodoEditor.js";
+import TodoList from "./component/TodoList.js";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <TodoEditor />
-      <div>Todo List</div>
+      <TodoList />
     </div>
   );
 }
---
+++ src/component/TodoList.js
@@ -0,0 +1,7 @@
+import "./TodoList.css";
+
+const TodoList = () => {
+  return <div className="TodoList">TodoList component</div>;
+};
+
+export default TodoList;
