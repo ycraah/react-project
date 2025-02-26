16155dae6712fbac6406a3878f7d4807f9f765b9
김예찬 <ycraah@gamil.com>
2025-02-26 15:34:30 +0900

    검색 기능 활성화화

--- src/component/TodoList.js
+++ src/component/TodoList.js
@@ -1,13 +1,28 @@
+import { useState } from "react";
 import TodoItem from "./TodoItem";
 import "./TodoList.css";
 
 const TodoList = ({ todo }) => {
+  const [search, setSearch] = useState("");
+  const onChangeSearch = (e) => {
+    setSearch(e.target.value);
+  };
+  const getSearchResult = () => {
+    return search === ""
+      ? todo
+      : todo.filter((it) => it.content.includes(search));
+  };
   return (
     <div className="TodoList">
       <h4>Todo List</h4>
-      <input className="searchbar" placeholder="검색어를 입력하세요" />
+      <input
+        value={search}
+        className="searchbar"
+        placeholder="검색어를 입력하세요"
+        onChange={onChangeSearch}
+      />
       <div className="list_wrapper">
-        {todo.map((it) => (
+        {getSearchResult().map((it) => (
           <TodoItem {...it} />
         ))}
       </div>
