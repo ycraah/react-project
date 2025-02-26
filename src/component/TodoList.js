927c55f9f889635cf2a0a74cc0923df24da9487e
김예찬 <ycraah@gamil.com>
2025-02-26 12:32:32 +0900

    TodoList 컴포넌트 생성

--- src/component/TodoList.js
+++ src/component/TodoList.js
@@ -1,3 +1,4 @@
+import TodoItem from "./TodoItem";
 import "./TodoList.css";
 
 const TodoList = () => {
@@ -5,6 +6,11 @@ const TodoList = () => {
     <div className="TodoList">
       <h4>Todo List</h4>
       <input className="searchbar" placeholder="검색어를 입력하세요" />
+      <div className="list_wrapper">
+        <TodoItem />
+        <TodoItem />
+        <TodoItem />
+      </div>
     </div>
   );
 };
