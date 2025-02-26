9d8d1df3511c32b10c299cbebe9f026c003ede55
김예찬 <ycraah@gamil.com>
2025-02-26 12:01:23 +0900

    header 컴포넌트 작성

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,5 +1,13 @@
+import "./Header.css";
+
 const Header = () => {
-  return <div className="Header">Header Component</div>;
+  var options = { year: "numeric", month: "long", day: "numeric" };
+  return (
+    <div className="Header">
+      <h3>오늘의 날짜는?</h3>
+      <h1>{new Date().toLocaleDateString("ko-KR", options)}</h1>
+    </div>
+  );
 };
 
 export default Header;
